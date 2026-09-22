03b4343bc1495235703c1c4a4a6c65e13d550ad2
P3dr0213 <Pdr05458@gmail.com>
2026-09-22 01:34:47 -0300

    ELEM-25 feat: Implementados cards de indicadores de conectividade
    Adicionados cards responsivos de KPIs em Dark Mode, skeleton de carregamento e integração com o serviço de status das estações.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,8 +1,10 @@
-import { Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+import { Platform } from '../pages/Platform/Platform'
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/platform" element={<Platform />} />
     </Routes>
   )
 }
