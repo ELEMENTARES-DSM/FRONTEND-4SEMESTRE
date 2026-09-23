2f86167f81e1b363377d51d5fa2233c8bf012b43
Pedro Miguel <pdr05458@gmail.com>
2026-09-23 08:06:49 -0300

    ELEM-26 feat: Tabela de Monitoramento e Badges de Tempo Decorrido

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
+import { Platform } from '../pages/Platform/Platform'
 import DevLib from '../pages/DevLib'
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/platform" element={<Platform />} />
       {/* <Route path="/" element={<ComponentShowcase />} /> */}
       <Route path="/devlib" element={<DevLib />} />
     </Routes>
